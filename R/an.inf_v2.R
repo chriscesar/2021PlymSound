a9b5fcc13f58b6778fc078fbea7a0e1e6859fd9e
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-04-18 16:38:53 +0100

    Calculate mean values across replicate samples

--- R/an.inf_v2.R
+++ R/an.inf_v2.R
@@ -34,20 +34,34 @@ df0 <- as_tibble(openxlsx::read.xlsx("data/in/Inf_Sed_all_years_Long_updated.xls
 
 df0 %>% rename("MatchedSite"=`2021.matched.site`) -> df0
 
-# filter out taxa to be 'discarded' and tidy up MatchedSite variable
+# 1. filter out taxa to be 'discarded' and tidy up MatchedSite variable
 df0 %>% 
   # change taxa flagged as -999 to values of 1. Consider removal of these taxa if required
-  mutate(Abund = ifelse(Abund == -999, 1, Abund)) %>% 
-  filter(.,Abund != 0) %>% #remove 'zero' abundances
+  # mutate(Abund = ifelse(Abund == -999, 1, Abund)) %>% 
+  # filter(.,Abund != 0) %>% #remove 'zero' abundances
   mutate(MatchedSite = paste0("PL_", substr(MatchedSite, 6, 7))) %>% #rename MatchedSite contents
   mutate(MatchedSiteYr = paste0(MatchedSite, "_", Year)) %>% #append Year value
   relocate(MatchedSiteYr, .after = MatchedSite) %>% #move new variable somewhere sensible
   #remove taxa flagged for removal
-  filter(.,!str_detect(Flag, "^Remove")) -> dfl
+  filter(.,!str_detect(Flag, "^Remove")) -> dfl0
 
 # next step:
-# generate mean values for taxa in replicate samples
-# (i.e., those with numeric values at end of dfl$Site variable)
+# 2. generate mean values for taxa in replicate samples
+# (i.e., those with non-numeric values at end of dfl$Site variable)
 
+## extract Replicates: rows with numeric value at end of Site name
+dfl_rep <- dfl0 %>% 
+  filter(str_sub(Site,-1) %in% letters) %>% # extract rows with numeric value at end of Site name
+  mutate(Site = substr(Site, 1, nchar(Site) - 1)) %>% #remove final character from Site
+  dplyr::select(.,-Taxon) %>% 
+  group_by(across(-Abund)) %>% 
+  summarise(Abund=mean(Abund),.groups = "drop")
+
+### set aside non-Replicate rows:
+dfl_nonrep <- dfl0 %>% 
+  filter(!str_sub(Site,-1) %in% letters) %>% 
+  dplyr::select(.,-Taxon)
+
+dfl <- rbind(dfl_nonrep,dfl_rep)
 
 # 3. 
